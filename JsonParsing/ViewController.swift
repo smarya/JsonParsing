04a96a76bdedbba36e2ac62db22a1411aee2e8ce
DurgaPrasad A <Durga.Prasad@maersk.com>
2019-04-04 15:51:18 +0530

    Initial Commit

---
+++ JsonParsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonParsing
+//
+//  Created by IBM-MOBILITY on 04/04/19.
+//  Copyright © 2019 DurgaPrasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
